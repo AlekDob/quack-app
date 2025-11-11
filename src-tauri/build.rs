1ef2a441024cfc7212a457442cfc06934040e998
Alek Dobrohotov <a.dobrohotov@cec.com>
2025-11-11 16:00:29 +0100

    Public upgrade and update

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,22 @@
 fn main() {
+    // Load .env file during build and expose variables to compilation
+    // This allows option_env!() to access these variables at compile time
+    if let Ok(_) = dotenvy::dotenv() {
+        // Variables are now available via std::env::var() during build
+        // and will be baked into the binary via option_env!() macro
+        println!("cargo:rerun-if-changed=../.env");
+
+        // Expose specific variables to the compilation
+        if let Ok(val) = std::env::var("GUMROAD_PRODUCT_ID") {
+            println!("cargo:rustc-env=GUMROAD_PRODUCT_ID={}", val);
+        }
+        if let Ok(val) = std::env::var("SUPABASE_URL") {
+            println!("cargo:rustc-env=SUPABASE_URL={}", val);
+        }
+        if let Ok(val) = std::env::var("SUPABASE_ANON_KEY") {
+            println!("cargo:rustc-env=SUPABASE_ANON_KEY={}", val);
+        }
+    }
+
     tauri_build::build()
 }
